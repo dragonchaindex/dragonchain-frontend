d9b2069df7e701354f87203371d9a87c2c061da3
Alexandre Melo <amelo@harlemnext.com>
2021-10-19 21:05:44 -0300

    Burn the fear

---
+++ src/views/NotFound.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import useTheme from 'hooks/useTheme'
+import styled from 'styled-components'
+import { Button, Heading, Text, LogoIcon } from '@pancakeswap/uikit'
+import { Link } from 'react-router-dom'
+import Page from 'components/Layout/Page'
+import { useTranslation } from 'contexts/Localization'
+
+const StyledNotFound = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 64px);
+  justify-content: center;
+`
+
+const NotFound = () => {
+  const { t } = useTranslation()
+  const { theme } = useTheme()
+
+  return (
+    <Page>
+      <StyledNotFound>
+        <LogoIcon width="64px" mb="8px" isDark={theme.isDark}  />
+        <Heading scale="xxl">404</Heading>
+        <Text mb="16px">{t('Oops, page not found.')}</Text>
+        <Button as={Link} to="/" scale="sm">
+          {t('Back Home')}
+        </Button>
+      </StyledNotFound>
+    </Page>
+  )
+}
+
+export default NotFound
