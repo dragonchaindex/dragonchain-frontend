a6516d46ea395caa56511290281121e812f042aa
Alexandre Melo <amelo@harlemnext.com>
2021-12-07 19:07:01 -0300

    stake

--- src/views/Pools/components/PoolCard/CardActions/StakeActions.tsx
+++ src/views/Pools/components/PoolCard/CardActions/StakeActions.tsx
@@ -28,8 +28,13 @@ const StakeAction: React.FC<StakeActionsProps> = ({
   const { stakingToken, stakingTokenPrice, stakingLimit, isFinished, userData } = pool
   const { t } = useTranslation()
   const stakedTokenBalance = getBalanceNumber(stakedBalance, stakingToken.decimals)
+
+  console.log(stakingToken)
+  console.log(stakingTokenPrice)
+  console.log(stakedTokenBalance)
+
   const stakedTokenDollarBalance = getBalanceNumber(
-    stakedBalance.multipliedBy(stakingTokenPrice),
+    stakedBalance.multipliedBy(1),
     stakingToken.decimals,
   )
 
